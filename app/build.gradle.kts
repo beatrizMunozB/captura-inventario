a6a6e74a0d93781e05b3ec1a1946d9e3335540f0
bmunoz <bmunoz@makita.cl>
2026-03-19 13:14:55 -0300

    se actualiza la version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.makita.InventarioDirigido"
         minSdk = 27
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.3.4"
+        versionCode = 9
+        versionName = "1.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -60,11 +60,13 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.3")
 
     implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended:1.6.0")
+   // implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation("androidx.compose.material:material-icons-extended")
     // implementation("androidx.compose.foundation:foundation:1.5.0")
     // implementation( "androidx.compose.foundation:foundation:1.3.1")
     // implementation("androidx.navigation:navigation-compose:2.5.3")
